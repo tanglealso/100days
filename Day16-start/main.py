e62c9fc8448000a6918b9f3a8af9fcd3bb0267ad
Hannah Brown <hannahbrown@Luiss-Air.attlocal.net>
2022-02-19 15:42:13 -0800

    Add Day16-oopCoffeeMaker

---
+++ Day16-start/main.py
@@ -0,0 +1,25 @@
+# import another_module
+# print(another_module.another_variable)
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my _screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+#table.set_style(align="l")
+table.align="l"
+print(table )
+
+
+
+
